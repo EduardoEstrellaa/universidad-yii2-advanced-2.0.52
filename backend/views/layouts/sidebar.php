d6de0efc92215b3a5c77c632c855b12fc4f1397a
EduardoEstrellaa <lalo.eaee@gmail.com>
2025-04-13 21:25:18 -0600

    [backend/views/layouts/sidebar.php] - Agregando a los miembros del equipo en el sidebar

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">UNIVERSIDAD</span>
     </a>
 
     <!-- Sidebar -->
@@ -10,10 +10,37 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= Yii::$app->request->baseUrl ?>/img/profile_pictures/edu.jpeg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">EDUARDO ESTRELLA</a>
+            </div>
+        </div>
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="<?= Yii::$app->request->baseUrl ?>/img/profile_pictures/jovis.jpeg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">JOHANA OLIVO</a>
+            </div>
+        </div>
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="<?= Yii::$app->request->baseUrl ?>/img/profile_pictures/gama.jpeg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">GEOVANNI PÉREZ</a>
+            </div>
+        </div>
+
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="<?= Yii::$app->request->baseUrl ?>/img/profile_pictures/cristo.jpeg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">CRISTOFER POOL</a>
             </div>
         </div>
 
